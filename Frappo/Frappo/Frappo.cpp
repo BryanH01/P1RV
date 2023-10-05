7548ca748d2eb9a3dc076732ff4d46d7150a12a2
Junka1XU <bixente.xu@eleves.ec-nantes.fr>
2023-10-05 08:54:07 +0200

    DT et FT
    version GPT

--- Frappo/Frappo/Frappo.cpp
+++ Frappo/Frappo/Frappo.cpp
@@ -3,11 +3,7 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    std::cout << "fini ?";
-}
+
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
